ac8609ab43abbce4065e966bc0f2c7cd00c94458
Shihab <s.bhuiyan@asthait.com>
2021-12-23 18:17:37 +0600

    initial commit

---
+++ pages/daily-update.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Daily status update page</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Daily-Update',
+  mounted: () => {
+      document.title="Daily Task Update";
+  }
+}
+</script>
---
+++ pages/personal-leave.vue
@@ -0,0 +1,14 @@
+
+
+<template>
+  <h1>Personal Leave form</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Personal-Leave',
+  mounted: () => {
+      document.title="Personal Leave";
+  }
+}
+</script>
