4619d07b0de717d8302e7fb59c9b74a0f16dd7a8
shihab-official <s.bhuiyan@asthait.com>
2022-02-24 18:28:45 +0600

    axios baseURL for production

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,8 @@ module.exports = {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:4002',
+    // baseURL: 'http://localhost:4002',
+    baseURL: 'https://astha-app-dir.herokuapp.com',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
