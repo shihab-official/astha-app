ea2a64f977c1bbe47872cc0cff444ef7535d6021
Shihab Bhuiyan <s.bhuiyan@asthait.com>
2022-03-14 00:52:54 +0600

    holiday calendar

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -4,10 +4,17 @@
       <h3 class="m-0">Calendar</h3>
     </div>
     <hr />
-    <a-calendar>
+    <a-calendar
+      :key="calendarKey"
+      :header-render="headerRender"
+      :valid-range="validRange"
+    >
       <ul slot="dateCellRender" slot-scope="value" class="events">
-        <li v-for="item in getListData(value)" :key="item.content">
-          <a-badge :status="item.type" :text="item.content" />
+        <li
+          v-for="item of getData(value)"
+          :key="item.content"
+        >
+          <div class="text-right" :class="{'text-red-500': item.type === 'holiday'}">{{ item.content }}</div>
         </li>
       </ul>
     </a-calendar>
@@ -15,6 +22,9 @@
 </template>
 
 <style>
+.ant-fullcalendar-fullscreen .ant-select-selection--single {
+  height: 35px;
+}
 .ant-fullcalendar-calendar-body {
   padding: 0;
 }
@@ -39,6 +49,12 @@
 .ant-fullcalendar-value {
   width: 100% !important;
   padding: 0 4px;
+}
+.ant-fullcalendar-today .ant-fullcalendar-value:before {
+    content: 'Today';
+    font-size: 92%;
+    color: #1890ff;
+    margin-right: 10px;
 }
 .ant-fullcalendar-content {
   height: unset !important;
@@ -70,48 +86,108 @@
 </style>
 
 <script>
+import moment from 'moment';
 import { mapGetters } from 'vuex';
 
 export default {
   name: 'calendar',
+  data() {
+    return {
+      calendarKey: 0,
+    };
+  },
   computed: {
     ...mapGetters('user', ['logs']),
     ...mapGetters('holiday', ['holidays']),
+    validRange: function () {
+      return [moment().startOf('year'), moment().endOf('year')];
+    },
+  },
+  watch: {
+    holidays: function () {
+      this.calendarKey++;
+    },
   },
   methods: {
-    getListData(value) {
-      let listData;
-      switch (value.date()) {
-        case 8:
-          listData = [
-            { type: 'warning', content: 'This is warning event.' },
-            { type: 'success', content: 'This is usual event.' },
-          ];
-          break;
-        case 10:
-          listData = [
-            { type: 'warning', content: 'This is warning event.' },
-            { type: 'success', content: 'This is usual event.' },
-            { type: 'error', content: 'This is error event.' },
-          ];
-          break;
-        case 15:
-          listData = [
-            { type: 'warning', content: 'This is warning event' },
-            {
-              type: 'success',
-              content: 'This is very long usual event。。....',
-            },
-            { type: 'error', content: 'This is error event 1.' },
-            { type: 'error', content: 'This is error event 2.' },
-            { type: 'error', content: 'This is error event 3.' },
-            { type: 'error', content: 'This is error event 4.' },
-          ];
-          break;
-        default:
+    getData: function (value) {
+      const items = [];
+      const date = value.format('DD-MMM-YYYY');
+      const holiday = this.holidays.find((holiday) => holiday.approved && holiday.date === date);
+
+      if (holiday) {
+        items.push({ type: 'holiday', content: holiday.title });
       }
-      return listData || [];
-    }
-  }
+
+      return items;
+    },
+
+    headerRender({ value, onChange }) {
+      const months = value.localeData().months();
+      const monthOptions = [];
+
+      for (let i = 0; i < months.length; i++) {
+        monthOptions.push(
+          <a-select-option class="month-item" key={`${i}`} value={i}>
+            {months[i]}
+          </a-select-option>
+        );
+      }
+      const monthIndex = value.month();
+
+      return (
+        <div style={{ padding: '10px' }}>
+          <div class="flex justify-between">
+            <span
+              class={`text-sm font-medium flex justify-center items-center rounded border border-solid w-20 ${
+                monthIndex > 0
+                  ? 'text-gray-400 hover:text-sky-500 cursor-pointer'
+                  : 'text-gray-200 cursor-default'
+              }`}
+              onClick={() => {
+                if (monthIndex > 0) {
+                  const newValue = value.clone();
+                  newValue.month(monthIndex - 1);
+                  onChange(newValue);
+                }
+              }}
+            >
+              Previous
+            </span>
+            <div style="font-size: 16px; font-weight: 500;">
+              <a-select
+                style="font: inherit; width: 165px; margin-right: 10px;"
+                dropdownMatchSelectWidth={false}
+                option-label-prop="label"
+                value={`${months[monthIndex]}, ${value.year()}`}
+                onChange={(selectedMonth) => {
+                  const newValue = value.clone();
+                  newValue.month(selectedMonth);
+                  onChange(newValue);
+                }}
+              >
+                {monthOptions}
+              </a-select>
+            </div>
+            <span
+              class={`text-sm font-medium flex justify-center items-center rounded border border-solid w-20 ${
+                monthIndex < 11
+                  ? 'text-gray-400 hover:text-sky-500 cursor-pointer'
+                  : 'text-gray-200 cursor-default'
+              }`}
+              onClick={() => {
+                if (monthIndex < 11) {
+                  const newValue = value.clone();
+                  newValue.month(monthIndex + 1);
+                  onChange(newValue);
+                }
+              }}
+            >
+              Next
+            </span>
+          </div>
+        </div>
+      );
+    },
+  },
 };
 </script>
