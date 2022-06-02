3ffdb117e7616ed67d565322e5633acf8476dd51
shihab-official <s.bhuiyan@asthait.com>
2022-06-02 10:37:50 +0600

    ui improvement; leave reason is not public anymore;

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@
               :style="{cursor: ($auth.user.admin || $auth.user.manager ? 'pointer' : '')}"
               @click="userLog($event, item.user_name)"
               :class="`w-full relative ${item.option === 0 ? 'option-0 text-right' : item.option === 1 ? 'option-1 text-left' : 'text-center'}`"
-              :title="`${item.option === 0 ? '1st half - ' : item.option === 1 ? '2nd half - ' : ''} ${item.detail}`"
+              :title="`${item.option === 0 ? '1st half - ' : item.option === 1 ? '2nd half - ' : ''}`"
             >
               <span class="relative z-10">{{ item.name }}</span>
             </a-tag>
